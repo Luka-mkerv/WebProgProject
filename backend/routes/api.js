2c48124ee3f3c888a87984e45fe8dbb4c07d9822
Mkerav <luka.mkervalishvili.1@iliauni.edu.ge>
2025-12-11 15:10:15 +0400

    Delete backend directory

--- backend/routes/api.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/shifts', (req, res) => {
-  res.json([{ id: 1, name: "Morning Shift" }, { id: 2, name: "Evening Shift" }]);
-});
-
-module.exports = router;
